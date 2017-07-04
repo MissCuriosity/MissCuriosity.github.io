cfbedc8417cefa06e72cdea363423030d40c9461
unknown <changye@thinkerx.com>
2017-07-04 18:27:24 +0800

    use jekyll

--- README.md
+++
@@ -1,4 +0,0 @@
-# MissCuriosity.github.io
-MY BLOG
-
-https://linghucong.js.org/2016/04/15/2016-04-15-hexo-github-pages-blog/
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cp -R package.json .deploy_git/MissCuriosity.github.io
-cd .deploy_git/MissCuriosity.github.io
-git add .
-git commit -m “add confix.yml”
-git push
