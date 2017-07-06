0a54892965a79343b5fab4996fd9b501dbb1c67c
changye <changye@thinkerx.com>
2017-07-06 16:46:22 +0800

    清除首页的部分链接，以及不需要的特效

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,12 +18,12 @@ $(function() {
 // All others
 $(document).ready(function() {
     // zoom in/zoom out animations
-    if ($(".container").hasClass('fadeOut')) {
-        $(".container").removeClass("fadeOut").addClass("fadeIn");
-    }
-    if ($(".wrapper").hasClass('fadeOut')) {
-        $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
-    }
+    // if ($(".container").hasClass('fadeOut')) {
+    //     $(".container").removeClass("fadeOut").addClass("fadeIn");
+    // }
+    // if ($(".wrapper").hasClass('fadeOut')) {
+    //     $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
+    // }
     $(".zoombtn").click(function() {
         $(".container").removeClass("fadeIn").addClass("fadeOut");
         $(".wrapper").removeClass("fadeIn").addClass("fadeOut");
